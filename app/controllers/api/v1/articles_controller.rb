47d3931ddb5f9b682df1d605379a0596c03bab48
AbdUlRahmanShawareb <bzsc1010@gmail.com>
2021-03-31 21:53:27 +0200

    add create article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,21 @@ module Api
         article = Article.find(params[:id])
         render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
       end
+
+      def create
+        article = Article.new(article_params)
+        if article.save
+          render json: { status: 'SUCCESS', message: 'Saved article', data: article }, status: :ok
+        else
+          render json: { status: 'ERROR', message: 'Article not saved', data: article.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def article_params
+        params.permit(:title, :body)
+      end
     end
   end
 end
