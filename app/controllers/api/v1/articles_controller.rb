796892b7f50627060918d96fd16291ef230c6d60
AbdUlRahmanShawareb <bzsc1010@gmail.com>
2021-03-31 21:43:03 +0200

    create the api, index, and show article

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        articles = Article.order('created_at DESC')
+        render json: { status: 'SUCCESS', message: 'Loaded articles', data: articles }, status: :ok
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'Loaded article', data: article }, status: :ok
+      end
+    end
+  end
+end
